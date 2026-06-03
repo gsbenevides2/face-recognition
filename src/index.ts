5e6be8758891e2ce0d70b34abd6ba26b20bb4c45
Bene Chan <bene-chan@gui.dev.br>
2026-06-03 02:35:40 +0000

    refactor: remove duplicated app logic and add getProjectInfo for OpenAPI

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,7 @@
 import { Elysia } from "elysia";
-import { cors } from "@elysiajs/cors";
-import { swagger } from "@elysiajs/openapi";
 import { healthcheckRoutes } from "./routes/healthcheck";
 
 export const app = new Elysia()
-  .use(cors())
-  .use(swagger({
-    documentation: {
-      info: { title: "Face Recognition API", version: "1.0.0" },
-    },
-  }))
   .use(healthcheckRoutes)
   .get("/", () => ({ message: "Face Recognition API" }));
 
