500be495081e0da03d3fba7f66107015fcb807c3
Guilherme da Silva Benevides <git@gui.dev.br>
2026-06-06 18:20:32 -0300

    fix: update release workflow to use job outputs for versioning; bump version to 0.0.15 in package.json

--- .github/workflows/release/docker.sh
+++ .github/workflows/release/docker.sh
@@ -21,14 +21,19 @@ VERSION_TAG="${IMAGE_URL}:${VERSION}"
 echo "Logando no Github Container Registry..."
 
 echo "${GITHUB_TOKEN}" | docker login ${REGISTRY} -u "${REPO_OWNER}" --password-stdin
-echo "Publicando imagem Docker com tag ${VERSION}..."
 
+echo "Construindo e publicando imagem Docker com tag ${VERSION}..."
 docker build -t "${VERSION_TAG}" .
+
+echo "Imagem Docker construída com tag ${VERSION_TAG}, marcando também como ${LATEST_TAG}, ${MINOR_TAG}, ${MAJOR_TAG}..."
 docker tag "${VERSION_TAG}" "${LATEST_TAG}"
 docker tag "${VERSION_TAG}" "${MINOR_TAG}"
 docker tag "${VERSION_TAG}" "${MAJOR_TAG}"
+
+echo "Publicando imagem Docker com tag ${VERSION}..."
 docker push "${VERSION_TAG}"
 docker push "${LATEST_TAG}"
 docker push "${MINOR_TAG}"
 docker push "${MAJOR_TAG}"
+
 echo "Imagem Docker publicada com tags: ${VERSION_TAG}, ${LATEST_TAG}, ${MINOR_TAG}, ${MAJOR_TAG}"
