b0800544f824358f826adbea2e513e0aefc71d1d
Guilherme da Silva Benevides <git@gui.dev.br>
2026-06-06 17:46:18 -0300

    fix: update health check function and improve error handling; bump version to 0.0.11 in package.json

--- healthcheck.ts
+++ healthcheck.ts
@@ -1,13 +1,16 @@
-try{
-    const response = await fetch('http://localhost:3000/api/api/healthcheck');
+async function healthCheck() {
+  try {
+    const response = await fetch("http://localhost:3000/api/api/healthcheck");
     if (response.ok) {
-        console.log('Health check successful');
-        process.exit(0);
+      console.log("Health check successful");
+      process.exit(0);
     } else {
-        console.error('Health check failed with status:', response.status);
-        process.exit(1);
+      console.error("Health check failed with status:", response.status);
+      process.exit(1);
     }
-}catch (error) {
-    console.error('Health check failed with error:', error);
+  } catch (error) {
+    console.error("Health check failed with error:", error);
     process.exit(1);
+  }
 }
+healthCheck();
