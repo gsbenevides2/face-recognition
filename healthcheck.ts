a0aa6e6fdec8eac3dadb278d42a7b25e1a72f853
Guilherme da Silva Benevides <git@gui.dev.br>
2026-06-06 17:43:46 -0300

    fix: improve health check implementation and update version to 0.0.10 in package.json

--- healthcheck.ts
+++ healthcheck.ts
@@ -1,2 +1,13 @@
-fetch('http://localhost:3000/api/healthcheck')
-    .then(r => r.status === 200 ? process.exit(0) : process.exit(1)).catch(()=>process.exit(1))
+try{
+    const response = await fetch('http://localhost:3000/api/api/healthcheck');
+    if (response.ok) {
+        console.log('Health check successful');
+        process.exit(0);
+    } else {
+        console.error('Health check failed with status:', response.status);
+        process.exit(1);
+    }
+}catch (error) {
+    console.error('Health check failed with error:', error);
+    process.exit(1);
+}
